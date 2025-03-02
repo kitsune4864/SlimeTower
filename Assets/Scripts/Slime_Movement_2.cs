bdbcd9a3e4d607e2fb4f7dbb2c81476dee5ed7e0
이서영 <88702441+WillowSY@users.noreply.github.com>
2025-03-02 18:13:10 +0900

    fix : 플레이어 컨트롤러 객체 분리

---
+++ Assets/Scripts/Slime_Movement_2.cs
@@ -0,0 +1,114 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor.Searcher;
+using UnityEngine;
+
+public class Slime_Movement_2 : MonoBehaviour
+{
+    private CharacterController cc;
+    private Rigidbody rb;
+    [SerializeField]
+    private float moveSpeed = 10f;
+    [SerializeField]
+    private float rotationSpeed = 10f;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField] 
+    private float fallForce = 2f;
+    [SerializeField] 
+    private int maxJumpForce = 200;
+    [SerializeField]
+    private float chargeJumpForce;
+    [SerializeField]
+    private bool isGrounded;
+    private Transform cameraTransform;
+    private Animator animator;
+    
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        rb = GetComponentInParent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        cameraTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        SlimeJump();
+    }
+
+    void FixedUpdate()
+    {
+        SlimeMove();
+        
+        SlimeFall();
+    }
+
+    private void SlimeMove()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = (cameraTransform.forward * v + cameraTransform.right * h);
+        dir = Vector3.ProjectOnPlane(dir, Vector3.up).normalized;
+        
+        rb.linearVelocity = dir * moveSpeed;
+        
+
+        if (dir != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        } // 이동값이 0 이 아닐떄 캐릭터를 카메라 방향으로 회전
+        
+        if (Mathf.Approximately(h, 0f) && Mathf.Approximately(v, 0f))
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+        }
+    }
+
+    private void SlimeJump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            if (jumpForce < maxJumpForce)
+            {
+                jumpForce += 10 * Time.deltaTime;
+            }
+            animator.SetBool("IsReady", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpForce = 100;
+            
+            animator.SetBool("IsReady", false);
+            animator.SetTrigger("IsJumping");
+        }
+    }
+
+    private void SlimeFall()
+    {
+        if (rb.linearVelocity.y <= 0)
+        {
+            rb.AddForce(Vector3.down * fallForce, ForceMode.Acceleration);
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Structure"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
+    }
+}
---
+++ Assets/Scripts/UIController_2.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController_2 : MonoBehaviour
+{
+    public PlayerStateManager _playerStateManager;
+    public GameObject leftClickUI;
+    public GameObject leftClickWhite;
+    public GameObject leftClickBlack;
+
+    private Coroutine curUICoroutine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("UIStructure"))
+        {
+            ShowLeftClickUI();
+            _playerStateManager.curStructureTransform = other.transform;
+            _playerStateManager.canSink = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("UIStructure"))
+        {
+            DisableLeftClickUI();
+        }
+        _playerStateManager.canSink = false;
+    }
+
+    public void ShowLeftClickUI()
+    {
+        leftClickUI.SetActive(true);
+        //FIXME : 추후 효과 추가
+        BlinkUI(leftClickWhite, leftClickBlack);
+    }
+
+    private void DisableLeftClickUI()
+    {
+        StopCoroutine(curUICoroutine);
+        leftClickUI.SetActive(false);
+    }
+
+
+    private void BlinkUI(GameObject obj1, GameObject obj2)
+    {
+        curUICoroutine = StartCoroutine(BlinkUICoroutine(obj1, obj2));
+    }
+    IEnumerator BlinkUICoroutine(GameObject obj1, GameObject obj2)
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(1f);
+        while (true)
+        {
+            if (obj1.activeSelf== true)
+            {
+                obj1.SetActive(false);
+                obj2.SetActive(true);
+            }
+            else
+            {
+                obj1.SetActive(true);
+                obj2.SetActive(false);
+            }
+            yield return waitTime;
+        }
+    }
+}
